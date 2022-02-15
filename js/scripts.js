5b5c16059fb09bb0f3f49507b1cf04ba872fd8dd
mathildeac <henneville.mathilde@hotmail.fr>
2022-02-15 19:02:41 +0000

    Change the code from 15th Feb - task 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log(hello world);
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
